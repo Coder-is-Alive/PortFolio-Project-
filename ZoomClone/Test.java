e6ae37095413d6884cf6695a252607aedcba87e4
Bibek Kshetri <bibekkshetri@Bibeks-MacBook-Air.local>
2026-02-13 22:42:49 +0545

    fixed some typos in Test files

--- ZoomClone/Test.java
+++ ZoomClone/Test.java
@@ -1,6 +1,6 @@
 public class Test {
  public static void main(String[] args) {
-System.out.println("hellop thsi is bibek kshetri");  
+System.out.println("hello this is bibek kshetri");  
 
   } 
 }
